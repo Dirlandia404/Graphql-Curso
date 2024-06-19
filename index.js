f3d3940e1ca1ec084bc1e16fbd60fe1c5ad1fe1a
Dirlandia404 <dirlandiaoliveira@alu.ufc.br>
2024-06-19 10:36:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { gql, ApolloServer } = require("apollo-server");
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => "Hello World!",
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
